0ba1b3e4df5cfa0dd3dd364e36965e7857e56ee0
Jeremy Dyer <jdye64@gmail.com>
2014-09-27 14:02:42 -0400

    Initial Commit

---
+++ Residence/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Residence
+//
+//  Created by Jeremy Dyer on 9/27/14.
+//  Copyright (c) 2014 Jeremy Dyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
