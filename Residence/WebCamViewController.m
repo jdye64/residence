2cbde51af102d25ac6d02b0d6df476d440bed7e5
Jeremy Dyer <jdye64@gmail.com>
2014-10-08 21:10:22 -0400

    Updates to play test audio files on a remote RPi device

--- Residence/WebCamViewController.m
+++ Residence/WebCamViewController.m
@@ -12,6 +12,9 @@
 
 @property(nonatomic, strong) MDWamp *wamp;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *coughBtn;
+@property (weak, nonatomic) IBOutlet UIButton *randomBtn;
+@property (weak, nonatomic) IBOutlet UIButton *lastBtn;
 
 @end
 
@@ -38,22 +41,100 @@
 // Called when client has connected to the server
 - (void) mdwamp:(MDWamp*)wamp sessionEstablished:(NSDictionary*)info {
     
+//    //Gets the list of devices from the session
+//    [wamp call:@"com.jeremydyer.residence.rpi.webcam.takesnapshot" args:nil kwArgs:nil complete:^(MDWampResult *result, NSError *error) {
+//        if (error == nil) {
+//            NSDictionary *jsonDict = (NSDictionary *) result.result;
+//            NSString *base64EncodedImage = jsonDict[@"image"];
+//            
+//            NSData *nsdataFromBase64String = [[NSData alloc]initWithBase64EncodedString:base64EncodedImage options:0];
+//            UIImage *image = [[UIImage alloc] initWithData:nsdataFromBase64String];
+//            self.imageView.image = image;
+//        } else {
+//            // handle the error
+//            NSLog(@"ERROR: %@", error);
+//        }
+//    }];
+    
+//    NSArray *args = @[@{@"source_url": @"https://s3.amazonaws.com/makeandbuild/courier/audio/1.wav"}];
+//    
+//    //Gets the list of devices from the session
+//    [wamp call:@"com.jeremydyer.residence.rpi.audio.play" args:args kwArgs:nil complete:^(MDWampResult *result, NSError *error) {
+//        if (error == nil) {
+//            NSDictionary *jsonDict = (NSDictionary *) result.result;
+//            NSString *base64EncodedImage = jsonDict[@"image"];
+//            
+//            NSData *nsdataFromBase64String = [[NSData alloc]initWithBase64EncodedString:base64EncodedImage options:0];
+//            UIImage *image = [[UIImage alloc] initWithData:nsdataFromBase64String];
+//            self.imageView.image = image;
+//        } else {
+//            // handle the error
+//            NSLog(@"ERROR: %@", error);
+//        }
+//    }];
+    
+}
+
+- (IBAction)play_cough:(id)sender {
+    NSArray *args = @[@{@"source_url": @"https://s3.amazonaws.com/makeandbuild/courier/audio/1.wav"}];
+    
     //Gets the list of devices from the session
-    [wamp call:@"com.jeremydyer.residence.rpi.webcam.takesnapshot" args:nil kwArgs:nil complete:^(MDWampResult *result, NSError *error) {
+    [self.wamp call:@"com.jeremydyer.residence.rpi.audio.play" args:args kwArgs:nil complete:^(MDWampResult *result, NSError *error) {
         if (error == nil) {
-            NSDictionary *jsonDict = (NSDictionary *) result.result;
-            NSString *base64EncodedImage = jsonDict[@"image"];
-            
-            NSData *nsdataFromBase64String = [[NSData alloc]initWithBase64EncodedString:base64EncodedImage options:0];
-            UIImage *image = [[UIImage alloc] initWithData:nsdataFromBase64String];
-            self.imageView.image = image;
+//            NSDictionary *jsonDict = (NSDictionary *) result.result;
+//            NSString *base64EncodedImage = jsonDict[@"image"];
+//            
+//            NSData *nsdataFromBase64String = [[NSData alloc]initWithBase64EncodedString:base64EncodedImage options:0];
+//            UIImage *image = [[UIImage alloc] initWithData:nsdataFromBase64String];
+//            self.imageView.image = image;
+            NSLog(@"Finished!");
         } else {
             // handle the error
             NSLog(@"ERROR: %@", error);
         }
     }];
-    
 }
+
+- (IBAction)play_random:(id)sender {
+    NSArray *args = @[@{@"source_url": @"https://s3.amazonaws.com/makeandbuild/courier/audio/2.wav"}];
+    
+    //Gets the list of devices from the session
+    [self.wamp call:@"com.jeremydyer.residence.rpi.audio.play" args:args kwArgs:nil complete:^(MDWampResult *result, NSError *error) {
+        if (error == nil) {
+//            NSDictionary *jsonDict = (NSDictionary *) result.result;
+//            NSString *base64EncodedImage = jsonDict[@"image"];
+//            
+//            NSData *nsdataFromBase64String = [[NSData alloc]initWithBase64EncodedString:base64EncodedImage options:0];
+//            UIImage *image = [[UIImage alloc] initWithData:nsdataFromBase64String];
+//            self.imageView.image = image;
+            NSLog(@"Finished!");
+        } else {
+            // handle the error
+            NSLog(@"ERROR: %@", error);
+        }
+    }];
+}
+
+- (IBAction)play_last:(id)sender {
+    NSArray *args = @[@{@"source_url": @"https://s3.amazonaws.com/makeandbuild/courier/audio/3.wav"}];
+    
+    //Gets the list of devices from the session
+    [self.wamp call:@"com.jeremydyer.residence.rpi.audio.play" args:args kwArgs:nil complete:^(MDWampResult *result, NSError *error) {
+        if (error == nil) {
+//            NSDictionary *jsonDict = (NSDictionary *) result.result;
+//            NSString *base64EncodedImage = jsonDict[@"image"];
+//            
+//            NSData *nsdataFromBase64String = [[NSData alloc]initWithBase64EncodedString:base64EncodedImage options:0];
+//            UIImage *image = [[UIImage alloc] initWithData:nsdataFromBase64String];
+//            self.imageView.image = image;
+            NSLog(@"Finished!");
+        } else {
+            // handle the error
+            NSLog(@"ERROR: %@", error);
+        }
+    }];
+}
+
 
 // Called when client disconnect from the server
 - (void) mdwamp:(MDWamp *)wamp closedSession:(NSInteger)code reason:(NSString*)reason details:(NSDictionary *)details {
